f90cb00ed33ef30ea9cddee8b8560f15303d08d0
Winfred Mutuku <winmutuku23@gmail.com>
2026-09-05 16:36:31 +0100

    Remove debug logging

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
 
-console.log("DEBUG: typeof app =", typeof app);
-console.log("DEBUG: app keys =", app && Object.keys(app));
-console.log("DEBUG: has listen =", typeof app?.listen);
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
